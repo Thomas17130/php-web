d29d25c09f6e6724fa5e04ed8406337c478dafec
THOMAS GUILLEMETTE <thomasguillemette06@gmail.com>
2025-06-23 15:57:02 +0200

    recuperation des données avec get

--- inscription.php
+++ inscription.php
@@ -14,20 +14,20 @@
             <legend>Données de connexion</legend>
         <div>
             <label for="username">Nom d'utilisateur</label>
-            <input type="text" id=username autocomplete="off">
+            <input type="text" id=username  autofocus name=username>
         </div>
         <div>
             <label for="password">Mot de passe</label>
-            <input type="password" id=password>
+            <input type="password" id=password name=password>
         </div>
         </fieldset>
         <div>
             <label for="age">Âge</label>
-            <input type="number" id=age min=0 max=150>
+            <input type="number" id=age min=0 max=150 name=age>
         </div>
         <div>
             <label for="ville">Ville</label>
-            <select id="ville">
+            <select id="ville" name=ville>
                 <option selected disabled>Merci de choisir votre ville de résidence</option>
                 <option value="Marseille">Marseille</option>
                 <option value="Lyon">Lyon</option>
@@ -36,19 +36,19 @@
         </div>
         <div>
             <label for="remarque">Remarque</label>
-            <textarea  id="remarque" cols="50" rows="5"></textarea>
+            <textarea  id="remarque" cols="50" rows="5" name=remarque></textarea>
         </div>
         <div>
             Genre
-            <input type="radio" name=genre>Homme 
-            <input type="radio" name=genre>Femme 
+            <input type="radio" name=genre value=homme>Homme 
+            <input type="radio" name=genre value=femme>Femme 
 
         </div>
         <div>
             Sport préféré
-            <input type="checkbox" name=sport>Football 
-            <input type="checkbox" name=sport>Handball 
-            <input type="checkbox" name=sport>Tennis 
+            <input type="checkbox" name=sport1 value=football>Football 
+            <input type="checkbox" name=sport2 value=handball>Handball 
+            <input type="checkbox" name=sport3 value=tennis>Tennis 
 
         </div>
         <div>
